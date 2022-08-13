6343161517a3e85d35864f8e26329418e0606cd0
mgeoher <mhernandezm85@miumg.edu.gt>
2022-08-13 09:41:32 -0600

    se crea la clase Libro

--- src/java/Clases/Libro.java
+++ src/java/Clases/Libro.java
@@ -9,5 +9,59 @@ package Clases;
  * @author mgeoh
  */
 public class Libro {
+    private String codigo;
+    private String nombre;
+    private String tipoPasta;
+    private String editorial;
+    private String año;
+    
+    public Libro(String codigo, String nombre, String tipoPasta, String editorial, String año){
+        this.codigo=codigo;
+        this.nombre=nombre;
+        this.tipoPasta=tipoPasta;
+        this.editorial=editorial;
+        this.año=año;
+        
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoPasta() {
+        return tipoPasta;
+    }
+
+    public void setTipoPasta(String tipoPasta) {
+        this.tipoPasta = tipoPasta;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getAño() {
+        return año;
+    }
+
+    public void setAño(String año) {
+        this.año = año;
+    }
     
 }
